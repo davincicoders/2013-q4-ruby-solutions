312265db70e0b92924ecbb88de4eacf9d98408ce
Daniel Stutzman <dtstutz@gmail.com>
2013-10-13 14:12:01 -0600

    Added 204, 205, and 206.

---
+++ 204-blood-pressures/db/seeds.rb
@@ -0,0 +1,14 @@
+Reading.create! \
+  name: "Ed",
+  diastolic: 90,
+  systolic: 140
+
+Reading.create! \
+  name: "Jan",
+  diastolic: 70,
+  systolic: 110
+
+Reading.create! \
+  name: "Tim",
+  diastolic: 80,
+  systolic: 120
---
+++ 206-athletes-x-races/db/seeds.rb
@@ -0,0 +1,8 @@
+Athlete.create! name: "Lisa", class_year: "2015"
+Athlete.create! name: "Eric", class_year: "2015"
+Athlete.create! name: "John", class_year: "2016"
+Athlete.create! name: "Amy",  class_year: "2016"
+
+Race.create! name: "100-meter dash", num_meters: 100
+Race.create! name: "400-meter dash", num_meters: 400
+Race.create! name: "10K run", num_meters: 10000
