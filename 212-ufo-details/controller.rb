6eb4f887b177f12e3e4ab789587e275b8e16a507
Daniel Stutzman <dtstutz@gmail.com>
2013-10-17 17:07:00 -0600

    Added 212 and 213.

---
+++ 212-ufo-details/controller.rb
@@ -0,0 +1,11 @@
+require './davinci-sinatra.rb'
+
+get "/" do
+  halt erb(:index)
+end
+
+get "/sightings/:id" do
+  id = params[:id]
+  @sighting = UfoSighting.find(id)
+  halt erb(:show)
+end
---
+++ 213-cities-details/controller.rb
@@ -0,0 +1,11 @@
+require './davinci-sinatra.rb'
+
+get "/" do
+  halt erb(:index)
+end
+
+get "/cities/:id" do
+  id = params[:id]
+  @city = WorldCity.find(id)
+  halt erb(:show)
+end
