32cd54b27154e7cdee17a35c5117d020084d42cb
Daniel Stutzman <dtstutz@gmail.com>
2013-10-17 22:52:50 -0600

    Added 216-routes-practice

---
+++ 216-routes-practice/controller.rb
@@ -0,0 +1,25 @@
+require './davinci-sinatra.rb'
+
+get "/" do
+  halt erb(:index)
+end
+
+get "/contact/form" do
+  halt erb(:contact_form)
+end
+
+get "/form-in-table" do
+  halt erb(:form_in_table)
+end
+
+get "/page/headers" do
+  halt erb(:headers)
+end
+
+get "/inline.php" do
+  halt erb(:inline)
+end
+
+get "/NestedLists.aspx" do
+  halt erb(:nested_lists)
+end
