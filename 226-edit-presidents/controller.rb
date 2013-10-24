64caf7cef34dadfafff505c9311008e316fe5918
Daniel Stutzman <dtstutz@gmail.com>
2013-10-24 15:57:11 -0600

    Added 226-edit-presidents.

---
+++ 226-edit-presidents/controller.rb
@@ -0,0 +1,26 @@
+require './davinci-sinatra.rb'
+
+get "/" do
+  @presidents = President.order(:id)
+  halt erb(:index)
+end
+
+get "/presidents/:id" do
+  id = params["id"]
+  @president = President.find(id)
+  halt erb(:edit)
+end
+
+post "/presidents/:id" do
+  id                           = params["id"]
+  @president                   = President.find(id)
+  @president.name              = params["name"]
+  @president.term_number       = params["term_number"]
+  @president.biography         = params["biography"]
+  @president.ended_term_early  = params["ended_term_early"]
+  @president.party             = params["party"]
+  @president.former_occupation = params["former_occupation"]
+  @president.birth_state_id    = params["birth_state_id"]
+  @president.save!
+  redirect "/"
+end
