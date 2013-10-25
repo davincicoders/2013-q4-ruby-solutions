7db3326509e6c51b5b669bcd47ec5e3fd0b01f24
Daniel Stutzman <dtstutz@gmail.com>
2013-10-24 20:58:41 -0600

    Added Sino-Tibetan language family.

---
+++ 227-edit-languages/controller.rb
@@ -0,0 +1,24 @@
+require './davinci-sinatra.rb'
+
+get "/" do
+  @languages = Language.order(:id)
+  halt erb(:index)
+end
+
+get "/languages/:id" do
+  id         = params["id"]
+  @language  = Language.find(id)
+  halt erb(:edit)
+end
+
+post "/languages/:id" do
+  id         = params["id"]
+  @language  = Language.find(id)
+  @language.name = params["name"]
+  @language.num_speakers = params["num_speakers"]
+  @language.language_family_id = params["language_family_id"]
+  @language.main_script = params["main_script"]
+  @language.has_arabic_script = params["has_arabic_script"]
+  @language.save!
+  redirect "/"
+end
