8b6b97bcc5bd37841e781fb5ae06d26585df1077
Daniel Stutzman <dtstutz@gmail.com>
2013-10-26 16:42:48 -0600

    Finished 226 and 227.

--- 226-edit-presidents/controller.rb
+++ 226-edit-presidents/controller.rb
@@ -5,6 +5,24 @@ get "/" do
   halt erb(:index)
 end
 
+get "/new_president" do
+  @president = President.new
+  halt erb(:edit)
+end
+
+post "/new_president" do
+  @president                   = President.new
+  @president.name              = params["name"]
+  @president.term_number       = params["term_number"]
+  @president.biography         = params["biography"]
+  @president.ended_term_early  = params["ended_term_early"]
+  @president.party             = params["party"]
+  @president.former_occupation = params["former_occupation"]
+  @president.birth_state_id    = params["birth_state_id"]
+  @president.save!
+  redirect "/"
+end
+
 get "/presidents/:id" do
   id = params["id"]
   @president = President.find(id)
--- 227-edit-languages/controller.rb
+++ 227-edit-languages/controller.rb
@@ -5,6 +5,22 @@ get "/" do
   halt erb(:index)
 end
 
+get "/new_language" do
+  @language = Language.new
+  halt erb(:edit)
+end
+
+post "/new_language" do
+  @language                    = Language.new
+  @language.name               = params["name"]
+  @language.num_speakers       = params["num_speakers"]
+  @language.language_family_id = params["language_family_id"]
+  @language.main_script        = params["main_script"]
+  @language.has_arabic_script  = params["has_arabic_script"]
+  @language.save!
+  redirect "/"
+end
+
 get "/languages/:id" do
   id         = params["id"]
   @language  = Language.find(id)
@@ -12,13 +28,13 @@ get "/languages/:id" do
 end
 
 post "/languages/:id" do
-  id         = params["id"]
-  @language  = Language.find(id)
-  @language.name = params["name"]
-  @language.num_speakers = params["num_speakers"]
+  id                           = params["id"]
+  @language                    = Language.find(id)
+  @language.name               = params["name"]
+  @language.num_speakers       = params["num_speakers"]
   @language.language_family_id = params["language_family_id"]
-  @language.main_script = params["main_script"]
-  @language.has_arabic_script = params["has_arabic_script"]
+  @language.main_script        = params["main_script"]
+  @language.has_arabic_script  = params["has_arabic_script"]
   @language.save!
   redirect "/"
 end
