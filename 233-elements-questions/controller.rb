181004389f2c9c34e5c76d07f1ca8cce041ed617
Daniel Stutzman <dtstutz@gmail.com>
2013-10-28 18:02:44 -0600

    Added 233-elements-questions

---
+++ 233-elements-questions/controller.rb
@@ -0,0 +1,17 @@
+require './davinci-sinatra.rb'
+
+get "/" do
+  halt erb(:questions)
+end
+
+post "/" do
+  if params[:name_of_element] != ""
+    element = Element.find_by!(name: params[:name_of_element])
+    @answer9 = element.number
+  end
+  if params[:number_of_element] != ""
+    element = Element.find_by!(number: params[:number_of_element])
+    @answer10 = element.name
+  end
+  halt erb(:answer_to_9_or_10)
+end
