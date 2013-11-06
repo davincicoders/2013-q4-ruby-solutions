54bba296f05712e5f57481765ebd61373de0ff9c
Daniel Stutzman <dtstutz@gmail.com>
2013-11-05 17:16:41 -0700

    Added 245-recipe-belongs-to.

---
+++ 245-recipe-belongs-to/controller.rb
@@ -0,0 +1,21 @@
+require './davinci-sinatra.rb'
+
+get "/" do
+  @recipes = Recipe.order(:id).all
+  @title = "All recipes"
+  halt erb(:index)
+end
+
+get "/recipe/:name" do
+  name = params["name"]
+  @recipe = Recipe.find_by(name: name)
+  halt erb(:show)
+end
+
+get "/by-author/:name" do
+  name = params["name"]
+  author = Author.find_by(first_name: name)
+  @recipes = author.recipes
+  @title = "Recipes by #{author.first_name}"
+  halt erb(:index)
+end
