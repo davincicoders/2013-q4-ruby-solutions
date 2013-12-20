f3fbb4c3358d4a2c4d46c78afe77802e37584c3a
Daniel Stutzman <dtstutz@gmail.com>
2013-12-20 09:43:14 -0700

    Refactored person form to use a custom helper for the fields.

--- 337-model-methods/app/helpers/application_helper.rb
+++ 337-model-methods/app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def full_name(p)
+    "#{p.first_name} #{p.last_name}"
+  end
+  
+  def person_field(name, form_field)
+    "<div class='form-group #{@person.errors[name] != [] && "has-error" }'>
+      #{label_tag name,
+        "#{name.to_s.humanize}
+        #{@person.errors[name].join(", ")}",
+        class: "control-label"}
+      #{form_field}
+    </div>".html_safe
+  end
+  
 end
